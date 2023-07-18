91d847a8c8d9a49c15b61526576e6922ec3079c8
Isaac <isaac.jimenez@utp.ac.pa>
2023-07-18 08:03:21 -0500

    new file:   src/main/java/com/cganadero_api/demo/repositories/GanadoRespositorie.java

---
+++ src/main/java/com/cganadero_api/demo/repositories/GanadoRespositorie.java
@@ -0,0 +1,12 @@
+package com.cganadero_api.demo.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cganadero_api.demo.models.Ganado;
+
+@Repository
+public interface GanadoRespositorie extends CrudRepository<Ganado, Long>
+{
+    
+}
