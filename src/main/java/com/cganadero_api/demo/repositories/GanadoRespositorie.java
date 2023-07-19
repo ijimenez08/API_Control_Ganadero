aabd8bc479925702ed6ee515711dac4b870a2550
Isaac <isaac.jimenez@utp.ac.pa>
2023-07-19 08:43:15 -0500

    eliminar y buscar 	modified:   src/main/java/com/cganadero_api/demo/repositories/GanadoRespositorie.java 	modified:   src/main/java/com/cganadero_api/demo/services/GanadoService.java

--- src/main/java/com/cganadero_api/demo/repositories/GanadoRespositorie.java
+++ src/main/java/com/cganadero_api/demo/repositories/GanadoRespositorie.java
@@ -1,5 +1,7 @@
 package com.cganadero_api.demo.repositories;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import com.cganadero_api.demo.models.Ganado;
 @Repository
 public interface GanadoRespositorie extends CrudRepository<Ganado, Long>
 {
-    //----------------------------------------------------------------
+    public abstract ArrayList<Ganado> finByNumGanado(Long numGanado);
     
 }
--- src/main/java/com/cganadero_api/demo/services/GanadoService.java
+++ src/main/java/com/cganadero_api/demo/services/GanadoService.java
@@ -1,6 +1,7 @@
 package com.cganadero_api.demo.services;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,29 @@ public class GanadoService
     public Ganado guardarGanados(Ganado ganado)
     {
         return ganadoRespositorie.save(ganado);
+    }
+
+    public Optional<Ganado> obtenerPorId(long id)
+    {
+        return ganadoRespositorie.findById(id);
+    }
+
+    public ArrayList<Ganado> obtenerPorNumGanado(long NumGanado)
+    {
+        return ganadoRespositorie.finByNumGanado(NumGanado);
+    } 
+
+    public boolean eliminarGanado(long id)
+    {
+        try 
+        {
+            ganadoRespositorie.deleteById(id);
+            return true;            
+        } 
+        catch (Exception err) 
+        {
+            return false;
+        }
     }
 
 }
